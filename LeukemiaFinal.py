81ef3daf5c9d3327d14f1ad5b8cdfb5e9a30a1ba
Conrad Li <conradli@login4.frontera.tacc.utexas.edu>
2021-03-21 23:01:20 -0500

    Fixed Heat maps in .py version

--- LeukemiaFinal.py
+++ LeukemiaFinal.py
@@ -6,6 +6,7 @@
 
 import matplotlib.pyplot as plt
 import matplotlib.image as mpimg
+from torchvision import transforms
 import numpy as np
 import json
 from pprint import pprint
@@ -21,7 +22,7 @@ print("done")
 
 # In[2]:
 
-
+defaults.device = torch.device('cuda:0')
 def dataset_creator(path):
   file_names = []
   for filename in os.listdir(path):
@@ -35,7 +36,7 @@ def dataset_creator(path):
 # In[3]:
 
 
-data_path = "/work/07925/alaukik/frontera/LeukemiaDataset/C-NMC_training_data/CombinedData"
+data_path = "/work/07925/alaukik/LeukemiaDataset/C-NMC_training_data/CombinedData"
 leuk_data = dataset_creator(data_path + '/all')
 no_leuk_data = dataset_creator(data_path + '/hem')
 
@@ -93,15 +94,15 @@ def image_to_arr_of_numpy_arr(data_array):
 # In[10]:
 
 
-image = mpimg.imread(data_path + '/all/UID_1_1_1_all.bmp')
-print(type)
-plt.imshow(image)
-plt.show()
+#image = mpimg.imread(data_path + '/all/UID_1_1_1_all.bmp')
+#print(type)
+#plt.imshow(image)
+#plt.show()
 
 
 # In[11]:
 
-
+tfms = [Normalize.from_stats(*imagenet_stats)]
 np.random.seed(32)
 data = ImageDataLoaders.from_lists(data_path, full_data, labels=labels_ls, valid_pct=0.2,
       batch_tfms= [*aug_transforms(),Normalize.from_stats(*imagenet_stats)], bs = 64, num_workers=4) 
@@ -111,7 +112,7 @@ data = ImageDataLoaders.from_lists(data_path, full_data, labels=labels_ls, valid
 # In[12]:
 
 
-data.show_batch(nrows=3, figsize=(7,8))
+#data.show_batch(nrows=3, figsize=(7,8))
 #data.label_list
 
 
@@ -130,27 +131,31 @@ learn = cnn_learner(data, models.resnet34, metrics=error_rate) #metrics = []
 
 # In[34]:
 
+#print("Reached training")
+#for i in range(0, 30):
+#  print("Epoch" + str(i))
+#  learn.fit_one_cycle(1)
+#  learn.save("con_stage-" + str(i))
 
-learn.fit_one_cycle(10)
 
 
 # In[35]:
 
 
-learn.save('stage-1')
-
+#path = learn.save('stage-1')
+#print(path)
 
 # In[36]:
 
 
-learn.recorder.plot_loss()
-learn.unfreeze()
+#learn.recorder.plot_loss()
+#learn.unfreeze()
 
 
 # In[37]:
 
 
-learn.lr_find()
+#learn.lr_find()
 
 
 # In[38]:
@@ -159,94 +164,103 @@ learn.lr_find()
 #figure out better way for lr
 #learn.recorder.plot(suggestion = True)
 #minlr = learn.recorder.min_grad_lr
-learn.fit_one_cycle(10,10e-5)
+#learn.fit_one_cycle(10,10e-5)
 
 
 # In[39]:
 
 
-learn.recorder.plot_loss()
+#learn.recorder.plot_loss()
 
 
 # In[40]:
 
 
-learn.save('stage-2')
+#learn.save('stage-2')
 
 
 # In[41]:
 
 
-learn.load('stage-2')
+#learn.load('stage-2')
 
 
 # In[42]:
 
+learn = learn.load('con_stage-24', device=torch.device("cuda:0"))
 
-interp = ClassificationInterpretation.from_learner(learn)
+print("Reached interp")
+#interp = ClassificationInterpretation.from_learner(learn)
 
 
 # In[43]:
 
 
-interp.plot_confusion_matrix()
+#interp.plot_confusion_matrix()
 
 
 # In[31]:
 
 
-from fastai.vision.widgets import *
+from fastai.vision.widgets import ImageClassifierCleaner
 
 
 # In[ ]:
 
-
-ds, idxs = DatasetFormatter().from_toplosses(learn)
-ImageCleaner(ds, idxs, data_path)
+#ImageClassifierCleaner(learn)
 
 
 # In[ ]:
 
+print("Reached Hooks")
 
-def visualize_cnn_by_cam(learn, data_index): ## learn is your learner, change this to whatever name you gave yours
-  x, _y = learn.data.valid_ds[data_index]
-  y = _y.data
-  if not isinstance(y, (list, np.ndarray)): # single label -> one hot encoding
-    y = np.eye(learn.data.valid_ds.c)[y]
-  m = learn.model.eval()
-  xb,_ = learn.data.one_item(x)
-  xb_im = Image(learn.data.denorm(xb)[0])
-  xb = xb.cuda()
 
-def hooked_backward(cat):
-    with hook_output(m[0]) as hook_a: 
-      with hook_output(m[0], grad=True) as hook_g:
-        preds = m(xb)
-        preds[0,int(cat)].backward()
-    return hook_a,hook_g
+class Hook():
+    def __init__(self, m):
+        self.hook = m.register_forward_hook(self.hook_func)   
+    def hook_func(self, m, i, o): self.stored = o.detach().clone()
+    def __enter__(self, *args): return self
+    def __exit__(self, *args): self.hook.remove()
 
-#matplotlib
-def show_heatmap(img, hm, label):
-    _,axs = plt.subplots(1, 2)
-    axs[0].set_title(label)
-    img.show(axs[0])
-    axs[1].set_title(f'CAM of {label}')
-    img.show(axs[1])
-    axs[1].imshow(hm, alpha=0.6, extent=(0,img.shape[1],img.shape[1],0),
-                  interpolation='bilinear', cmap='magma');
-    plt.show()
-    #pytorch
-  for y_i in np.where(y > 0)[0]:
-    hook_a,hook_g = hooked_backward(cat=y_i)
-    acts = hook_a.stored[0].cpu()
-    grad = hook_g.stored[0][0].cpu()
-    grad_chan = grad.mean(1).mean(1)
-    mult = (acts*grad_chan[...,None,None]).mean(0)
-    show_heatmap(img=xb_im, hm=mult, label=str(learn.data.valid_ds.y[data_index]))
-#learn.data.valid.ds - translate
+# Hook class
+class HookBwd():
+    def __init__(self, m):
+        self.hook = m.register_backward_hook(self.hook_func)   
+    def hook_func(self, m, gi, go): self.stored = go[0].detach().clone()
+    def __enter__(self, *args): return self
+    def __exit__(self, *args): self.hook.remove()
 
-num_leuk = len (leuk_data)
-num_no_leuk = len (no_leuk_data)       
+
+def visualize_cnn_by_cam(learn, index):
+  x, y = learn.dls.valid_ds[index]
+  x.save("./output/image_" + str(index) + ".png")
+  x = transforms.ToTensor()(x).unsqueeze(0)
+  #for t in tfms:
+  #  x = t(x, split_idx=0)
+  cls = y
+  with HookBwd(learn.model[0]) as hookg:
+      with Hook(learn.model[0]) as hook:
+          output = learn.model.eval()(x)
+          act = hook.stored
+      output[0,cls].backward()
+      grad = hookg.stored
+
+  # Take the mean of the gradients
+  w = grad[0].mean(dim=[1,2], keepdim=True)
+  cam_map = (w * act[0]).sum(0)
+
+  # Show the plot
+  x_dec = TensorImage(data.train.decode((x,))[0][0])
+  _,ax = plt.subplots()
+  x_dec.show(ctx=ax)
+  ax.imshow(cam_map.detach().cpu(), alpha=0.6, extent=(0,450,450,0),
+                interpolation='bilinear', cmap='magma')
+  plt.savefig("./output/heatmap" +  str(index) + ".png")
+  
+
+visualize_cnn_by_cam(learn, 2)
+#num_leuk = len (leuk_data)
+#num_no_leuk = len (no_leuk_data)       
 for idx in range(10): ## Range can be whatever you want, or select a specific image of interest given its index
     visualize_cnn_by_cam(learn, idx)
 
